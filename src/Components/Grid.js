12d381395b06f5ff7675c20de40ddded1453be3a
Karim Makhloufi <karim.makhloufi.paris@gmail.com>
2021-12-10 14:47:13 +0100

    Player

--- src/Components/Grid.js
+++ src/Components/Grid.js
@@ -2,6 +2,7 @@ import { useState, Fragment } from "react";
 import Box from "./Box";
 
 const Grid = () => {
+  const [player, setPlayer] = useState(0);
   const [boxes, setBoxes] = useState([
     [0, 0, 0],
     [0, 0, 0],
@@ -9,18 +10,27 @@ const Grid = () => {
   ]);
 
   const handleClick = (idx, idx2) => {
-    console.log(idx, idx2);
-    const currentBoxes = [...boxes];
-    currentBoxes[idx][idx2] = 1;
-    setBoxes(currentBoxes);
-  }
+    if (boxes[idx][idx2] === 0) {
+      console.log(idx, idx2);
+      const currentBoxes = [...boxes];
+      currentBoxes[idx][idx2] = (player % 2) + 1;
+      setPlayer(player + 1);
+      setBoxes(currentBoxes);
+    } else {
+      alert("non");
+    }
+  };
 
   return (
     <div>
       {boxes.map((line, idx) => (
         <Fragment key={idx}>
           {line.map((el, idx2) => (
-            <Box key={idx2} value={el} handleClick={() => handleClick(idx, idx2)} />
+            <Box
+              key={idx2}
+              value={el}
+              handleClick={() => handleClick(idx, idx2)}
+            />
           ))}
           <br></br>
         </Fragment>
